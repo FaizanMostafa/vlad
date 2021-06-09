bc4885d65962ec83653b58999ee93cd4860e9d29
Faizan Mostafa <faizanmustafa324@gmail.com>
2021-06-09 11:22:33 +0500

    redux setup

--- theprocessguysv2/src/App.js
+++ theprocessguysv2/src/App.js
@@ -1,39 +1,16 @@
 import React from "react";
-import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import TheProcessGuys from "./pages/TheProcessGuys";
-// import QuestionairePage from "./pages/Questionaire";
-import Login from "./pages/Login";
-import Register from "./pages/register";
-import ForgotPassword from "./pages/ForgotPassword";
-import NavBar from "./navbar/navBar";
-import Footer from "./footer/footer";
-import SingleSubmission from "./pages/singleSubmission";
-import MultiSubmission from "./pages/multipleSubmission";
-import PacketSubmissionPage from "./pages/packetSubmissionPage";
-import MemberDashboard from "./pages/Dashboard";
-import UpdateProfilePage from "./pages/UpdateProfile";
+import { Provider } from 'react-redux';
+import Navigation from "./navigation";
+import store from "./redux/store";
 import './App.css';
-import "mdbreact"
+import "mdbreact";
 
 export default function App() {
   return (
     <div className="homepage">
-      <NavBar></NavBar>
-      <Router>
-      <Switch>
-      <Route exact path='/' component = {TheProcessGuys} />
-      <Route path = '/single-submission' component = {SingleSubmission} />
-      <Route path = '/multi-submission' component = {MultiSubmission} />
-      <Route path = '/packet-submission-page' component = {PacketSubmissionPage} />
-      <Route path = '/login' component = {Login} />
-      <Route path = '/register' component = {Register} />
-      <Route path = '/forgot-password' component = {ForgotPassword} />
-      <Route path = '/member-dashboard' component = {MemberDashboard} />
-      <Route path = '/update-profile' component = {UpdateProfilePage} />
-      {/* <Route path = '/questionaire' component = {QuestionairePage} /> */}
-      </Switch>
-      </Router>
-      <Footer></Footer>
+      <Provider store={store} >
+        <Navigation />
+      </Provider>
     </div>
   );
 }
