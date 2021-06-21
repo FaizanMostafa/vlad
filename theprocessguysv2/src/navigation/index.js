4a5a6e17efff30678d0d44716032335be6c17f9b
VictorWong87 <39934171+VictorWong87@users.noreply.github.com>
2021-06-21 01:02:47 -0700

    Added the admin dashboard in case you needed it

--- theprocessguysv2/src/navigation/index.js
+++ theprocessguysv2/src/navigation/index.js
@@ -16,6 +16,7 @@ import LoadingPage from "../pages/Loading";
 import UpdateProfilePage from "../pages/UpdateProfile";
 import { fetchUser } from "../redux/actions/auth";
 import ViewCases from '../pages/viewCases';
+import AdminDashboard from '../pages/adminDashboard';
 
 const Navigation = (props) => {
 
@@ -48,6 +49,7 @@ const Navigation = (props) => {
           <Route path = '/multi-submission' component = {MultiSubmission} />
           <Route path = '/packet-submission-page' component = {PacketSubmissionPage} />
           <Route path = '/view-cases' component = {ViewCases} />
+          <Route path = '/admin-dashboard' component = {AdminDashboard} />
           <Route path = '/login' component = {Login} />
           <Route path = '/register' component = {Register} />
           <Route path = '/forgot-password' component = {ForgotPassword} />
---
+++ theprocessguysv2/src/pages/adminDashboard.js
@@ -0,0 +1,75 @@
+import React from "react"
+import { Card } from "react-bootstrap"
+import { Link} from "react-router-dom"
+
+function AdminDashboard() {
+
+
+  return (
+    <>
+      <Card className="homepage">
+
+        <Card.Body className="text-center">
+
+          <h2 className="text-center mb-4">Admin Dashboard</h2>
+          <div>
+            <img className="img profile-pic" alt="img"/>
+          </div>
+          <br></br>
+            <div>E-mail: `{}`</div>
+            <br></br>
+            <div>Name: `{}`</div>
+            <br></br>
+            <div>Address: `{}`</div>
+            <br></br>
+            <div>Phone Number: `{}`</div>
+            <br></br>
+
+          <Link to="/update-profile" className="btn btn-primary w-50 justify-content-center">
+            Update Profile
+          </Link>
+          <br></br>
+          <Link to="/view-all-cases" className="btn btn-primary w-50 justify-content-center">
+            View All Cases
+          </Link>
+          <br></br>
+          <Link to="/view-all-clients" className="btn btn-primary w-50 justify-content-center">
+            View All Accounts
+          </Link>
+          <br></br>
+          <Link to="/review-case-submissions" className="btn btn-primary w-50 justify-content-center">
+            Review New Case Submissions
+          </Link>
+          <br></br>
+          <Link to="/terms-of-service" className="btn btn-primary w-50 justify-content-center">
+            Terms Of Service
+          </Link>
+          <br></br>
+          <Link to="/contact-request-submission" className="btn btn-primary w-50 justify-content-center">
+            Contact Request Submission
+          </Link>
+          <br></br>
+        </Card.Body>
+
+      </Card>
+      <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+    </>
+  )
+}
+
+export default AdminDashboard;
