f2a38cfb4e2f5e58fc833a56dde31d8b2e90f25c
Faizan Mostafa <faizanmustafa324@gmail.com>
2021-07-03 20:15:18 +0500

    User details shown in member dashboard

--- theprocessguysv2/src/pages/Dashboard.js
+++ theprocessguysv2/src/pages/Dashboard.js
@@ -1,38 +1,60 @@
-import React from "react"
-import { Card } from "react-bootstrap"
-import { Link} from "react-router-dom"
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import { Link} from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
-function Dashboard() {
-
+const Dashboard = (props) => {
+  const user = useSelector(state => state.auth.user);
 
   return (
     <>
       <Card className="homepage">
-
         <Card.Body className="text-center">
-
           <h2 className="text-center mb-4">Member Dashboard</h2>
           <div>
-            <img className="img profile-pic" alt="img"/>
+            <img src={user.profilePicture} className="img profile-pic" alt="img"/>
           </div>
           <br></br>
-            <div>E-mail: `{}`</div>
+            <div>E-mail: {user.email}</div>
             <br></br>
-            <div>Name: `{}`</div>
+            {
+              user.userType === "personal"
+                &&
+                  <>
+                    <div>Name: {user.name}</div>
+                    <br></br>
+                  </>
+            }
+            {
+              user.userType === "business"
+                &&
+                  <>
+                    <div>Business Name: {user.firmName}</div>
+                    <br></br>
+                  </>
+            }
+            {
+              user.userType === "attorney"
+                &&
+                  <>
+                    <div>Attorney Name: {user.name} </div>
+                    <br></br>
+                  </>
+            }
+            {
+              user.userType === "paralegal"
+                &&
+                  <>
+                    <div>Paralegal Name: {user.name}</div>
+                    <br></br>
+                  </>
+            }
+            <div>Fax Number: {user.faxNo}</div>
             <br></br>
-            {/*if business shows this*/}
-            <div>Business Name: `{}`</div>
+            {/* What does that mean? */}
+            <div>Personal: {}</div>
             <br></br>
-            {/*if attorney show this*/}
-            <div>Attorney Name: `{}` </div>
-            <br></br>
-            {/*if paralegal show this*/}
-            <div>Paralegal Name: `{}`</div>
-            <br></br>
-            <div>Phone Number: `{}`</div>
-            <br></br>
-            <div>Personal: `{}`</div>
-            <br></br>
+            {/*  */}
 
           <Link to="/update-profile" className="btn btn-primary w-50 justify-content-center">
             Update Profile
